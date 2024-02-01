38ce4dd54c585514388650f0c392928dee8c839b
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2024-02-01 21:45:21 +0200

    added todo

---
+++ gandriz-nba/src/routes/app/tournament/games/games.jsx
@@ -0,0 +1,2 @@
+// TODO: make this according to design
+// TODO: save server data to local storage for faster loading
--- gandriz-nba/src/routes/app/tournament/teams/teams.jsx
+++ gandriz-nba/src/routes/app/tournament/teams/teams.jsx
@@ -1,4 +1,3 @@
-// TODO: TournamentTeams according to design
 import React from "react";
 import { useParams } from "react-router-dom";
 
