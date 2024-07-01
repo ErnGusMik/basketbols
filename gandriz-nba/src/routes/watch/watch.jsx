cbf9c14600f681aef6a634007c91a9de028ffe04
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2024-07-01 13:24:41 +0300

    added background change overlay

--- gandriz-nba/src/routes/watch/watch.jsx
+++ gandriz-nba/src/routes/watch/watch.jsx
@@ -5,6 +5,13 @@ import React from "react";
 import "./watch.css";
 
 const Watch = () => {
+
+
+
+    const changeBackground = (newID) => {
+        document.querySelector('.watch__cont').classList.add(newID);
+    };
+
     return (
         <div className="watch__cont">
             <div className="teamsCont">
@@ -46,6 +53,21 @@ const Watch = () => {
                 </div>
             </div>
             <i class="fa-solid fa-pen editBtn"></i>
+            <div className="customizeOverlay">
+                <div className="customizeOverlayData">
+                    <i className="fa-solid fa-close"></i>
+                    <h3>Pielāgot</h3>
+                    <div className="displaysCont">
+                        <div className="display active" id="defaultDisplay"></div>
+                        <div className="display" id="display2"></div>
+                        <div className="display" id="display3"></div>
+                        <div className="display" id="display4"></div>
+                        <div className="display" id="display5"></div>
+                        <div className="display" id="display6"></div>
+                        <div className="display" id="display7"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
