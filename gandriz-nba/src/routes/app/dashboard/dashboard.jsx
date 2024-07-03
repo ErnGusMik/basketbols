8fa0597496d1d1d0680cef71dd255880dafd4f56
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2024-07-03 23:08:07 +0300

    continued dashboard

--- gandriz-nba/src/routes/app/dashboard/dashboard.jsx
+++ gandriz-nba/src/routes/app/dashboard/dashboard.jsx
@@ -1,29 +1,89 @@
+// TODO: Put data in the dashboard components
+// TODO: Customize greeting
+// TODO: Move on to settings (at least add a coming soon page and logout option)
+
 import React from "react";
 
 import "./dashboard.css";
 
-import mainImg from './main.jpg';
+import mainImg from "./main.jpg";
 import Button from "../../../components/button/button";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
+    // Function to decode JWTs
+    const parseJwt = (token) => {
+        const base64Url = token.split(".")[1];
+        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        const jsonPayload = decodeURIComponent(
+            window
+                .atob(base64)
+                .split("")
+                .map((c) => {
+                    return (
+                        "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2)
+                    );
+                })
+                .join("")
+        );
+
+        return JSON.parse(jsonPayload);
+    };
+
+    const getData = async () => {
+        const idTokenEncoded = localStorage.getItem("id_token");
+        const idToken = parseJwt(idTokenEncoded);
+        console.log(idToken.sub);
+        const tournamentRequest = await fetch(
+            "http://localhost:8080/api/" + idToken.sub + "/tournaments",
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("id_token")}`,
+                },
+            }
+        );
+        const tournamentData = await tournamentRequest.json();
+        console.log(tournamentData);
+    };
+
+    React.useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <div className="dashboard">
             <h1>Sveiks, Ernests! Uzspēlējam?</h1>
             <div className="component">
-                d
-                <img src={mainImg} alt="Tournament logo" className="logo" />
-                <div className="tournamentData">
-                    <h2>Skolas čempis 2023</h2>
-                    <div className="infoRow">
-                        <i className="fa-solid fa-location-dot" style={{fontWeight: 600}}></i>
-                        <p>Ogre</p>
+                <div className="divider">
+                    <Link to="/app/tournaments/170">
+                        <img
+                            src={mainImg}
+                            alt="Tournament logo"
+                            className="logo"
+                        />
+                    </Link>
+                    <div className="tournamentData">
+                        <Link to="/app/tournaments/170">
+                            <h2>Skolas čempis 2023</h2>
+                        </Link>
+                        <div className="infoRow">
+                            <i
+                                className="fa-solid fa-location-dot"
+                                style={{ fontWeight: 600 }}
+                            ></i>
+                            <p>Ogre</p>
+                        </div>
+                        <div className="infoRow">
+                            <i
+                                className="fa-solid fa-calendar"
+                                style={{ fontWeight: 300 }}
+                            ></i>
+                            <p>01/12/23 - 10/12/2023</p>
+                        </div>
+                        <p>Organizē Ogres 1. vidusskola</p>
                     </div>
-                    <div className="infoRow">
-                        <i className="fa-solid fa-calendar" style={{fontWeight: 300}}></i>
-                        <p>01/12/23 - 10/12/2023</p>
-                    </div>
-                    <p>Organizē Ogres 1. vidusskola</p>
                 </div>
                 <div className="game">
                     <h3>Nākamā spēle</h3>
@@ -44,9 +104,19 @@ const Dashboard = () => {
                     </Link>
                 </div>
             </div>
-            <Link to="/app/new-tournament">
-                <Button text="Jauns turnīrs" icon={<i className="fa-solid fa-plus" style={{fontWeight: 300, marginRight: '5px'}}></i>} />
-            </Link>
+            <div className="newTournamentBtn">
+                <Link to="/app/tournaments/new">
+                    <Button
+                        text="Jauns turnīrs"
+                        icon={
+                            <i
+                                className="fa-solid fa-plus"
+                                style={{ fontWeight: 300, marginRight: "5px" }}
+                            ></i>
+                        }
+                    />
+                </Link>
+            </div>
         </div>
     );
 };
